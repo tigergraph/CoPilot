d48f658de2e725e27ec1ec4164bf2c01867e486b
Parker Erickson <parker.erickson@tigergraph.com>
2023-12-06 17:33:18 -0600

    tests(openai gpt3.5): fix test env variable

--- test_openai_gpt3.5-turbo.py
+++ test_openai_gpt3.5-turbo.py
@@ -15,7 +15,7 @@ if USE_WANDB:
 class TestWithOpenAI(CommonTests, unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        os.environ["LLM_CONFIG"] = "./configs/openai_llm_config.json"
+        os.environ["LLM_CONFIG"] = "./configs/openai_gpt3.5-turbo_config.json"
         from main import app
         cls.client = TestClient(app)
         cls.llm_service = "openai_gpt-3.5-turbo-1106"
