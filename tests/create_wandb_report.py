aabda127b2e1626777ca7d3726832acef5703f3d
Parker Erickson <parker.erickson@tigergraph.com>
2024-01-16 08:36:23 -0600

    tests(wandb report): update report to include not wrong percentage

--- tests/create_wandb_report.py
+++ tests/create_wandb_report.py
@@ -11,7 +11,7 @@ report = wr.Report(
 
 python_filter = "branch == '"+Repository('.').head.shorthand+"' and commit_hash == '"+Repository('.').head.peel(Commit).id.hex+"'"
 
-llm_service_bar_plot = wr.PanelGrid(
+acc_llm_service_bar_plot = wr.PanelGrid(
     runsets=[wr.Runset(project="llm-eval-sweep", name="LLM Service Grouping", groupby=["llm_service"]).set_filters_with_python_expr(python_filter)],
     panels = [
         wr.BarPlot(
@@ -20,12 +20,12 @@ llm_service_bar_plot = wr.PanelGrid(
             groupby="llm_service",
             groupby_aggfunc="mean",
             groupby_rangefunc="stddev",
-            layout={'w': 24, 'h': 9}  # change the layout!
+            layout={'w': 24, 'h': 16}  # change the layout!
         )
     ]
 )
 
-question_type_bar_plot = wr.PanelGrid(
+acc_question_type_bar_plot = wr.PanelGrid(
     runsets=[wr.Runset(project="llm-eval-sweep", name="Question Type Grouping", groupby=["question_type"]).set_filters_with_python_expr(python_filter)],
     panels = [
         wr.BarPlot(
@@ -34,13 +34,13 @@ question_type_bar_plot = wr.PanelGrid(
             groupby="question_type",
             groupby_aggfunc="mean",
             groupby_rangefunc="stddev",
-            layout={'w': 24, 'h': 9}  # change the layout!
+            layout={'w': 24, 'h': 16}  # change the layout!
         )
     ]
 )
 
 
-parallel_cords = wr.PanelGrid(
+acc_parallel_cords = wr.PanelGrid(
     runsets=[wr.Runset(project="llm-eval-sweep").set_filters_with_python_expr(python_filter)],
     panels = [
         wr.ParallelCoordinatesPlot(
@@ -50,7 +50,51 @@ parallel_cords = wr.PanelGrid(
                 wr.PCColumn(metric="c::question_type"),
                 wr.PCColumn(metric="Accuracy"),
             ],
-            layout={'w': 24, 'h': 9}  # change the layout!
+            layout={'w': 24, 'h': 16}  # change the layout!
+        )
+    ]
+)
+
+nrp_llm_service_bar_plot = wr.PanelGrid(
+    runsets=[wr.Runset(project="llm-eval-sweep", name="LLM Service Grouping", groupby=["llm_service"]).set_filters_with_python_expr(python_filter)],
+    panels = [
+        wr.BarPlot(
+            title="Average Not Wrong Percent by LLM Service",
+            metrics=["Not Wrong Percent"],
+            groupby="llm_service",
+            groupby_aggfunc="mean",
+            groupby_rangefunc="stddev",
+            layout={'w': 24, 'h': 16}  # change the layout!
+        )
+    ]
+)
+
+nrp_question_type_bar_plot = wr.PanelGrid(
+    runsets=[wr.Runset(project="llm-eval-sweep", name="Question Type Grouping", groupby=["question_type"]).set_filters_with_python_expr(python_filter)],
+    panels = [
+        wr.BarPlot(
+            title="Average Not Wrong Percent by Question Type",
+            metrics=["Not Wrong Percent"],
+            groupby="question_type",
+            groupby_aggfunc="mean",
+            groupby_rangefunc="stddev",
+            layout={'w': 24, 'h': 16}  # change the layout!
+        )
+    ]
+)
+
+
+nrp_parallel_cords = wr.PanelGrid(
+    runsets=[wr.Runset(project="llm-eval-sweep").set_filters_with_python_expr(python_filter)],
+    panels = [
+        wr.ParallelCoordinatesPlot(
+            columns=[
+                wr.PCColumn(metric="c::llm_service"),
+                wr.PCColumn(metric="c::dataset"),
+                wr.PCColumn(metric="c::question_type"),
+                wr.PCColumn(metric="Not Wrong Percent"),
+            ],
+            layout={'w': 24, 'h': 16}  # change the layout!
         )
     ]
 )
@@ -59,12 +103,12 @@ table = wr.PanelGrid(
     runsets=[wr.Runset(project="llm-eval-sweep").set_filters_with_python_expr(python_filter)],
     panels = [
         wr.WeavePanelSummaryTable(table_name="qa_results",
-            layout={'w': 24, 'h': 9}  # change the layout!
+            layout={'w': 24, 'h': 16}  # change the layout!
         )
     ]
 )
 
-report.blocks = [llm_service_bar_plot, question_type_bar_plot, parallel_cords, table]
+report.blocks = [acc_llm_service_bar_plot, acc_question_type_bar_plot, acc_parallel_cords, table, nrp_llm_service_bar_plot, nrp_question_type_bar_plot, nrp_parallel_cords]
 report.save()
 
 print(report.url)
