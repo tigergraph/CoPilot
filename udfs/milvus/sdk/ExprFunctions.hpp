655a4a214fd129c2cc13542f64a9c3a20f6176ff
Ahmed Albadri <ahmed@tigergraph.com>
2024-03-22 12:22:21 -0400

    GML-1533 - Cleaned up the old sdk implementation and leftover files

--- udfs/milvus/sdk/ExprFunctions.hpp
+++
@@ -1,8 +0,0 @@
-#include "ExprUtil.hpp"
-
-inline std::vector<std::pair<std::string, std::string>> searchInMilvus(const std::string milvus_host, const int64_t milvus_port, const std::string& collection_name, 
-            const std::string& vector_field_name, const std::string& vertex_id_field_name, const std::vector<float>& query_vector, const std::string& metric_type, const int64_t top_k) {
-
-    MilvusUtil milvus_util(milvus_host, milvus_port);
-    return milvus_util.search(collection_name, vector_field_name, vertex_id_field_name, query_vector, metric_type, top_k);
-}
