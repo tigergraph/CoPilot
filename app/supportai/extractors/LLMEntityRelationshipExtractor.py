cf46e1fd5a1682e250a47f63c5cee4164986091e
Parker Erickson <parker.erickson@tigergraph.com>
2024-03-25 13:06:04 -0500

    feat(er extraction): add defined types to extract

--- app/supportai/extractors/LLMEntityRelationshipExtractor.py
+++ app/supportai/extractors/LLMEntityRelationshipExtractor.py
@@ -1,13 +1,17 @@
 from app.llm_services import LLM_Model
 from app.supportai.extractors.BaseExtractor import BaseExtractor
 from app.py_schemas import KnowledgeGraph
+from typing import List
 import json
 
 
 
 class LLMEntityRelationshipExtractor(BaseExtractor):
-    def __init__(self, llm_service: LLM_Model):
+    def __init__(self, llm_service: LLM_Model, allowed_vertex_types: List[str] = None, allowed_edge_types: List[str] = None, strict_mode: bool = False):
         self.llm_service = llm_service
+        self.allowed_vertex_types = allowed_vertex_types
+        self.allowed_edge_types = allowed_edge_types
+        self.strict_mode = strict_mode
 
     def _extract_kg_from_doc(self, doc, chain, parser):
         try:
@@ -24,7 +28,7 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
             formatted_rels = []
             for rels in json_out["rels"]:
                 if isinstance(rels["source"], str) and isinstance(rels["target"], str):
-                    formatted_rels.append(rels)
+                    formatted_rels.append({"source": rels["source"], "target": rels["target"], "type": rels["relation_type"].replace(" ", "_").upper(), "definition": rels["definition"]})
                 elif isinstance(rels["source"], dict) and isinstance(rels["target"], str):
                     formatted_rels.append({"source": rels["source"]["id"], "target": rels["target"], "type": rels["relation_type"].replace(" ", "_").upper(), "definition": rels["definition"]})
                 elif isinstance(rels["source"], str) and isinstance(rels["target"], dict):
@@ -36,6 +40,13 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
             formatted_nodes = []
             for node in json_out["nodes"]:
                 formatted_nodes.append({"id": node["id"], "type": node["node_type"].replace(" ", "_").capitalize(), "definition": node["definition"]})
+
+            # filter relationships and nodes based on allowed types
+            if self.strict_mode:
+                if self.allowed_vertex_types:
+                    formatted_nodes = [node for node in formatted_nodes if node["type"] in self.allowed_vertex_types]
+                if self.allowed_edge_types:
+                    formatted_rels = [rel for rel in formatted_rels if rel["type"] in self.allowed_edge_types]
             return {"nodes": formatted_nodes, "rels": formatted_rels}
         except:
             print("Error Processing: ", out)
@@ -45,15 +56,20 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
         from langchain.prompts import ChatPromptTemplate
         from langchain.output_parsers import PydanticOutputParser
         parser = PydanticOutputParser(pydantic_object=KnowledgeGraph)
-        prompt = ChatPromptTemplate.from_messages(
-                            [(
-                            "system", self.llm_service.entity_relationship_extraction_prompt),
-                                ("human", "Tip: Make sure to answer in the correct format and do "
-                                            "not include any explanations. "
-                                            "Use the given format to extract information from the "
-                                            "following input: {input}"),
-                                ("human", "Mandatory: Make sure to answer in the correct format, specified here: {format_instructions}"),
-                            ])
+        prompt = [("system", self.llm_service.entity_relationship_extraction_prompt),
+                  ("human", "Tip: Make sure to answer in the correct format and do "
+                            "not include any explanations. "
+                            "Use the given format to extract information from the "
+                            "following input: {input}"),
+                  ("human", "Mandatory: Make sure to answer in the correct format, specified here: {format_instructions}")]
+        if self.allowed_vertex_types or self.allowed_edge_types:
+            prompt = prompt.append(("human", "Mandatory: Make sure to use the following allowed types:"))
+        if self.allowed_vertex_types:
+            prompt = prompt.append(("human", f"Allowed Vertex Types: {self.allowed_vertex_types}"))
+        if self.allowed_edge_types:
+            prompt = prompt.append(("human", f"Allowed Edge Types: {self.allowed_edge_types}"))
+
+        prompt = ChatPromptTemplate(prompt)
         chain = prompt | self.llm_service.model #| parser
         er =  self._extract_kg_from_doc(document, chain, parser)
         return er
