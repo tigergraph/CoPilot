64692a1c2f6307919bba1e0e5c35c07723c701fa
Ahmed Albadri <ahmed@tigergraph.com>
2024-06-06 02:27:57 -0400

    Removed config symlinks too.  minimized the build script

--- scripts/build_copilot.sh
+++ scripts/build_copilot.sh
@@ -1,14 +1,2 @@
-# setup
-buildPath="build/copilot"
 cd ..
-rm -rf build
-mkdir build
-
-# copy assets into build
-cp -R copilot $buildPath
-rm $buildPath/app/configs 
-cp -R configs $buildPath/app
-
-# docker build
 docker build -t copilot -f copilot/Dockerfile .
-rm -rf build
--- scripts/build_ecc.sh
+++ scripts/build_ecc.sh
@@ -1,15 +1,2 @@
-# setup
-buildPath="build/ecc"
 cd ..
-rootPath=`pwd`
-rm -rf build
-mkdir build
-
-# copy assets into build
-cp -R eventual-consistency-service $buildPath
-rm $buildPath/app/configs
-cp -R configs $buildPath/app
-
-# docker build
 docker build -t ecc -f eventual-consistency-service/Dockerfile .
-rm -rf build
