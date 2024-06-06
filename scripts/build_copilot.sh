d950b65376f5ccc12a5cc84f8be994c79d4eedb6
Ahmed Albadri <ahmed@tigergraph.com>
2024-06-06 02:35:57 -0400

    Readded the configs

--- scripts/build_copilot.sh
+++ scripts/build_copilot.sh
@@ -1,2 +1,13 @@
+# setup
+buildPath="build/copilot"
 cd ..
+rm -rf build
+mkdir build
+
+# copy assets into build
+cp -R copilot $buildPath
+cp -R configs $buildPath/app
+
+# docker build
 docker build -t copilot -f copilot/Dockerfile .
+rm -rf build
