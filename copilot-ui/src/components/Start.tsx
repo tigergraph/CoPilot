5df8c5d68580215159dc78edb494314da2dc99f6
RobRossmiller-TG <165701656+RobRossmiller-TG@users.noreply.github.com>
2024-06-26 15:27:06 -0400

    questions based on graph name

--- copilot-ui/src/components/Start.tsx
+++ copilot-ui/src/components/Start.tsx
@@ -2,17 +2,29 @@ import { FC, useState } from "react";
 import { HiOutlineChatBubbleOvalLeft } from "react-icons/hi2";
 import { useTheme } from "@/components/ThemeProvider";
 
-const questions = [
-  { title: "How many transactions are there?" },
-  { title: "Tell me about transaction fraud." },
-  { title: "Describe flow of one transaction." },
-  { title: "How TigerGraph can help me?" },
-  { title: "How to use visualization correctly?" },
-  // { title: "How to detect fraud in transactions?" },
-  // { title: "What is William Torres' ID?" },
-  // { title: "What's his email?" },
-  // {title:"How do I get a count of vertices in Python?"}
-];
+console.log(localStorage.getItem('selectedGraph'));
+
+const questions =
+  localStorage.getItem('selectedGraph') === 'pyTigerGraphRAGpyTigerGraphRAG'
+    ? [
+        {title: 'How many transactions are there?'},
+        {title: 'Tell me about transaction fraud.'},
+        {title: 'Describe flow of one transaction.'},
+        {title: 'How TigerGraph can help me?'},
+        // { title: "How to use visualization correctly?" },
+        // { title: "How to detect fraud in transactions?" },
+        // { title: "What is William Torres' ID?" },
+        // { title: "What's his email?" },
+        // {title:"How do I get a count of vertices in Python?"}
+      ]
+    : [
+        {title: 'What can pyTG do?'},
+        {title: 'How do I authenticate with the DB?'},
+        {title: 'How do I run PageRank?'},
+        {title: 'Help me initialize a NodePiece model'},
+        {title: 'Can I load data with pyTG?'},
+        {title: 'Write a short python snippet to run a loading job'},
+      ];
 
 interface Start {
   props: any;
