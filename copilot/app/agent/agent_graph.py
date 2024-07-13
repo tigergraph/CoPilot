5b9af2c89bd5e0661c2509fdf2c662a7496fd1a7
Lu Zhou <luzhou@TigerGrGPJ4M29R.attlocal.net>
2024-07-12 23:31:26 -0700

    GML-1822 remove hallucination and usefullness check and cypher support for regession test

--- copilot/app/agent/agent_graph.py
+++ copilot/app/agent/agent_graph.py
@@ -47,7 +47,7 @@ class TigerGraphAgentGraph:
         embedding_store,
         mq2s_tool,
         gen_func_tool,
-        # cypher_gen_tool=None,
+        cypher_gen_tool=None,
         enable_human_in_loop=False,
         q: Q = None,
         supportai_retriever="hnsw_overlap",
@@ -59,7 +59,7 @@ class TigerGraphAgentGraph:
         self.embedding_store = embedding_store
         self.mq2s = mq2s_tool
         self.gen_func = gen_func_tool
-        # self.cypher_gen = cypher_gen_tool
+        self.cypher_gen = cypher_gen_tool
         self.enable_human_in_loop = enable_human_in_loop
         self.q = q
 
@@ -156,37 +156,37 @@ class TigerGraphAgentGraph:
         state["lookup_source"] = "inquiryai"
         return state
 
-    # def generate_cypher(self, state):
-    #     """
-    #     Run the agent cypher generator.
-    #     """
-    #     self.emit_progress("Generating the Cypher to answer your question")
-    #     cypher = self.cypher_gen._run(state["question"])
+    def generate_cypher(self, state):
+        """
+        Run the agent cypher generator.
+        """
+        self.emit_progress("Generating the Cypher to answer your question")
+        cypher = self.cypher_gen._run(state["question"])
 
-    #     response = self.db_connection.gsql(cypher)
-    #     response_lines = response.split("\n")
-    #     try:
-    #         json_str = "\n".join(response_lines[1:])
-    #         response_json = json.loads(json_str)
-    #         state["context"] = {
-    #             "answer": response_json["results"][0],
-    #             "cypher": cypher,
-    #             "reasoning": "The following OpenCypher query was executed to answer the question. {}".format(
-    #                 cypher
-    #             ),
-    #         }
-    #     except:
-    #         state["context"] = {
-    #             "error": True,
-    #             "cypher": cypher,
-    #         }
-    #         if state["error_history"] is None:
-    #             state["error_history"] = []
+        response = self.db_connection.gsql(cypher)
+        response_lines = response.split("\n")
+        try:
+            json_str = "\n".join(response_lines[1:])
+            response_json = json.loads(json_str)
+            state["context"] = {
+                "answer": response_json["results"][0],
+                "cypher": cypher,
+                "reasoning": "The following OpenCypher query was executed to answer the question. {}".format(
+                    cypher
+                ),
+            }
+        except:
+            state["context"] = {
+                "error": True,
+                "cypher": cypher,
+            }
+            if state["error_history"] is None:
+                state["error_history"] = []
             
-    #         state["error_history"].append({"error_message": response, "error_step": "generate_cypher"})
+            state["error_history"].append({"error_message": response, "error_step": "generate_cypher"})
 
-    #     state["lookup_source"] = "cypher"
-    #     return state
+        state["lookup_source"] = "cypher"
+        return state
 
     def hnsw_overlap_search(self, state):
         """
@@ -357,46 +357,47 @@ class TigerGraphAgentGraph:
         """
         Run the agent hallucination check.
         """
-        self.emit_progress("Checking the response is relevant")
-        step = TigerGraphAgentHallucinationCheck(self.llm_provider)
+        # self.emit_progress("Checking the response is relevant")
+        # step = TigerGraphAgentHallucinationCheck(self.llm_provider)
 
-        try:
-            context_data_str = json.dumps(state["context"]["result"])
-            # logger.info(f"context_data_str: {context_data_str}")
-        except (TypeError, ValueError) as e:
-            logger.error(f"Failed to serialize context to JSON: {e}")
-            raise ValueError("Invalid context data format. Unable to convert to JSON.")
-        hallucinations = step.check_hallucination(
-            state["answer"].natural_language_response, context_data_str
-        )
-        logger.info(f"hallucination checker")
-        logger.info(f"answer: {state['answer'].natural_language_response}")
-        logger.info(f"context: {context_data_str}")
-        logger.info(f"if grounded: {hallucinations}")
-        if hallucinations.score == "yes":
-            self.emit_progress(DONE)
-            return "grounded"
-        else:
-            return "hallucination"
+        # try:
+        #     context_data_str = json.dumps(state["context"]["result"])
+        #     # logger.info(f"context_data_str: {context_data_str}")
+        # except (TypeError, ValueError) as e:
+        #     logger.error(f"Failed to serialize context to JSON: {e}")
+        #     raise ValueError("Invalid context data format. Unable to convert to JSON.")
+        # hallucinations = step.check_hallucination(
+        #     state["answer"].natural_language_response, context_data_str
+        # )
+        # logger.info(f"hallucination checker")
+        # logger.info(f"answer: {state['answer'].natural_language_response}")
+        # logger.info(f"context: {context_data_str}")
+        # logger.info(f"if grounded: {hallucinations}")
+        # if hallucinations.score == "yes":
+        #     self.emit_progress(DONE)
+        #     return "grounded"
+        # else:
+        #     return "hallucination"
+        return "grounded"
 
     def check_answer_for_usefulness(self, state):
         """
         Run the agent usefulness check.
         """
-        step = TigerGraphAgentUsefulnessCheck(self.llm_provider)
+        # step = TigerGraphAgentUsefulnessCheck(self.llm_provider)
 
-        usefulness = step.check_usefulness(
-            state["question"], state["answer"].natural_language_response
-        )
-        logger.info(f"usefulness checker")
-        logger.info(f"question: {state['question']}")
-        logger.info(f"answer: {state['answer'].natural_language_response}")
-        logger.info(f"if useful: {usefulness}")
-        if usefulness.score == "yes":
-            return "useful"
-        else:
-            return "not_useful"
-        # return "useful"
+        # usefulness = step.check_usefulness(
+        #     state["question"], state["answer"].natural_language_response
+        # )
+        # logger.info(f"usefulness checker")
+        # logger.info(f"question: {state['question']}")
+        # logger.info(f"answer: {state['answer'].natural_language_response}")
+        # logger.info(f"if useful: {usefulness}")
+        # if usefulness.score == "yes":
+        #     return "useful"
+        # else:
+        #     return "not_useful"
+        return "useful"
 
     def check_answer_for_usefulness_and_hallucinations(self, state):
         """
@@ -490,21 +491,22 @@ class TigerGraphAgentGraph:
             self.check_state_for_generation_error,
             {"error": "rewrite_question", "success": "generate_answer"},
         )
+
         if self.supportai_enabled:
             self.workflow.add_conditional_edges(
-                "generate_answer",
+                "generate_answer", 
                 self.check_answer_for_usefulness_and_hallucinations,
                 {
                     "hallucination": "rewrite_question",
                     "grounded": END,
-                    "not_useful": "rewrite_question",
+                    # "not_useful": "rewrite_question",
                     "inquiryai_not_useful": "supportai",
                     "supportai_not_useful": "map_question_to_schema",
                 },
             )
         else:
             self.workflow.add_conditional_edges(
-                "generate_answer",
+                "generate_answer", 
                 self.check_answer_for_usefulness_and_hallucinations,
                 {
                     "hallucination": "rewrite_question",
