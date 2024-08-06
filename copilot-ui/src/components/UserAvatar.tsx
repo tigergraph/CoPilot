4717cca741abf033d9b923110195526fc4413c27
Bill Shi <bill.shi@tigergraph.com>
2024-08-06 15:54:37 -0700

    fix(UserAvatar): replace mock user picture with user icon

--- copilot-ui/src/components/UserAvatar.tsx
+++ copilot-ui/src/components/UserAvatar.tsx
@@ -9,7 +9,7 @@ export const UserAvatar: FC<UserAvatarProps> = ({ props }) => {
     <>
       {props}
       <img
-        src="./avatar.svg"
+        src="./user.svg"
         className='h-[52px] w-[52px] relative inline-block rounded-full overflow-hidden md:h-11 md:w-11"'
       />
     </>
