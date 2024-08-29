e76fbd3f873c54d0ff74bb9e7b98c907f4e7b6c4
Parker Erickson <parker.erickson@tigergraph.com>
2024-08-29 15:44:19 -0500

    update async/sync llm relationship extraction

--- common/extractors/LLMEntityRelationshipExtractor.py
+++ common/extractors/LLMEntityRelationshipExtractor.py
@@ -20,42 +20,13 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
         self.allowed_edge_types = allowed_relationship_types
         self.strict_mode = strict_mode
 
-    async def _extract_kg_from_doc(self, doc, chain, parser) -> list[GraphDocument]:
-        """
-        returns:
-        {
-            "nodes": [
-                {
-                    "id": "str",
-                    "type": "string",
-                    "definition": "string"
-                }
-            ],
-            "rels": [
-                {
-                    "source":{
-                        "id": "str",
-                        "type": "string",
-                        "definition": "string"
-                    }
-                    "target":{
-                        "id": "str",
-                        "type": "string",
-                        "definition": "string"
-                    }
-                    "definition"
-                }
-            ]
-        }
-        """
-
+    async def _aextract_kg_from_doc(self, doc, chain, parser) -> list[GraphDocument]:
         try:
             out = await chain.ainvoke(
                 {"input": doc, "format_instructions": parser.get_format_instructions()}
             )
         except Exception as e:
-            print("Error: ", e)
-            return {"nodes": [], "rels": []}
+            return [GraphDocument(nodes=[], relationships=[], source=Document(page_content=doc))]
         try:
             if "```json" not in out.content:
                 json_out = json.loads(out.content.strip("content="))
@@ -150,10 +121,112 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
             return [GraphDocument(nodes=nodes, relationships=relationships, source=Document(page_content=doc))]
 
         except:
-            print("Error Processing: ", out)
-        return [GraphDocument(nodes=[], relationships=[], source=Document(page_content=doc))]
+            return [GraphDocument(nodes=[], relationships=[], source=Document(page_content=doc))]
+        
+    def _extract_kg_from_doc(self, doc, chain, parser) -> list[GraphDocument]:
+        try:
+            out = chain.invoke(
+                {"input": doc, "format_instructions": parser.get_format_instructions()}
+            )
+        except Exception as e:
+            return [GraphDocument(nodes=[], relationships=[], source=Document(page_content=doc))]
+        try:
+            if "```json" not in out.content:
+                json_out = json.loads(out.content.strip("content="))
+            else:
+                json_out = json.loads(
+                    out.content.split("```")[1].strip("```").strip("json").strip()
+                )
 
-    async def document_er_extraction(self, document):
+            formatted_rels = []
+            for rels in json_out["rels"]:
+                if isinstance(rels["source"], str) and isinstance(rels["target"], str):
+                    formatted_rels.append(
+                        {
+                            "source": rels["source"],
+                            "target": rels["target"],
+                            "type": rels["relation_type"].replace(" ", "_").upper(),
+                            "definition": rels["definition"],
+                        }
+                    )
+                elif isinstance(rels["source"], dict) and isinstance(
+                    rels["target"], str
+                ):
+                    formatted_rels.append(
+                        {
+                            "source": rels["source"]["id"],
+                            "target": rels["target"],
+                            "type": rels["relation_type"].replace(" ", "_").upper(),
+                            "definition": rels["definition"],
+                        }
+                    )
+                elif isinstance(rels["source"], str) and isinstance(
+                    rels["target"], dict
+                ):
+                    formatted_rels.append(
+                        {
+                            "source": rels["source"],
+                            "target": rels["target"]["id"],
+                            "type": rels["relation_type"].replace(" ", "_").upper(),
+                            "definition": rels["definition"],
+                        }
+                    )
+                elif isinstance(rels["source"], dict) and isinstance(
+                    rels["target"], dict
+                ):
+                    formatted_rels.append(
+                        {
+                            "source": rels["source"]["id"],
+                            "target": rels["target"]["id"],
+                            "type": rels["relation_type"].replace(" ", "_").upper(),
+                            "definition": rels["definition"],
+                        }
+                    )
+                else:
+                    raise Exception("Relationship parsing error")
+            formatted_nodes = []
+            for node in json_out["nodes"]:
+                formatted_nodes.append(
+                    {
+                        "id": node["id"],
+                        "type": node["node_type"].replace(" ", "_").capitalize(),
+                        "definition": node["definition"],
+                    }
+                )
+
+            # filter relationships and nodes based on allowed types
+            if self.strict_mode:
+                if self.allowed_vertex_types:
+                    formatted_nodes = [
+                        node
+                        for node in formatted_nodes
+                        if node["type"] in self.allowed_vertex_types
+                    ]
+                if self.allowed_edge_types:
+                    formatted_rels = [
+                        rel
+                        for rel in formatted_rels
+                        if rel["type"] in self.allowed_edge_types
+                    ]
+        
+            nodes = []
+            for node in formatted_nodes:
+                nodes.append(Node(id=node["id"],
+                                  type=node["type"],
+                                  properties={"description": node["definition"]}))
+            relationships = []
+            for rel in formatted_rels:
+                relationships.append(Relationship(source=Node(id=rel["source"], type=rel["source"],
+                                                  properties={"description": rel["definition"]}),
+                                                  target=Node(id=rel["target"], type=rel["target"],
+                                                  properties={"description": rel["definition"]}), type=rel["type"]))
+
+            return [GraphDocument(nodes=nodes, relationships=relationships, source=Document(page_content=doc))]
+
+        except:
+            return [GraphDocument(nodes=[], relationships=[], source=Document(page_content=doc))]
+        
+    async def adocument_er_extraction(self, document):
         from langchain.prompts import ChatPromptTemplate
         from langchain.output_parsers import PydanticOutputParser
 
@@ -187,13 +260,51 @@ class LLMEntityRelationshipExtractor(BaseExtractor):
             prompt.append(("human", f"Allowed Edge Types: {self.allowed_edge_types}"))
         prompt = ChatPromptTemplate.from_messages(prompt)
         chain = prompt | self.llm_service.model  # | parser
-        er = await self._extract_kg_from_doc(document, chain, parser)
+        er = await self._aextract_kg_from_doc(document, chain, parser)
+        return er
+
+
+    def document_er_extraction(self, document):
+        from langchain.prompts import ChatPromptTemplate
+        from langchain.output_parsers import PydanticOutputParser
+
+    
+        parser = PydanticOutputParser(pydantic_object=KnowledgeGraph)
+        prompt = [
+            ("system", self.llm_service.entity_relationship_extraction_prompt),
+            (
+                "human",
+                "Tip: Make sure to answer in the correct format and do "
+                "not include any explanations. "
+                "Use the given format to extract information from the "
+                "following input: {input}",
+            ),
+            (
+                "human",
+                "Mandatory: Make sure to answer in the correct format, specified here: {format_instructions}",
+            ),
+        ]
+        if self.allowed_vertex_types or self.allowed_edge_types:
+            prompt.append(
+                (
+                    "human",
+                    "Tip: Make sure to use the following types if they are applicable. "
+                    "If the input does not contain any of the types, you may create your own.",
+                )
+            )
+        if self.allowed_vertex_types:
+            prompt.append(("human", f"Allowed Node Types: {self.allowed_vertex_types}"))
+        if self.allowed_edge_types:
+            prompt.append(("human", f"Allowed Edge Types: {self.allowed_edge_types}"))
+        prompt = ChatPromptTemplate.from_messages(prompt)
+        chain = prompt | self.llm_service.model  # | parser
+        er = self._extract_kg_from_doc(document, chain, parser)
         return er
 
     def extract(self, text):
         return self.document_er_extraction(text)
     
     async def aextract(self, text) -> list[GraphDocument]:
-        return await self.document_er_extraction(text)
+        return await self.adocument_er_extraction(text)
     
 
